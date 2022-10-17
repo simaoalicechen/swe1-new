a5f74711a39c6fb98ae30fced8353ed4a4aba3e2
Alice Chen <alicechen005@gmail.com>
2022-10-17 13:53:33 -0400

    Reformatted according to the black file

--- polls/tests.py
+++ polls/tests.py
@@ -4,11 +4,8 @@ from django.utils import timezone
 from .models import Question
 from django.utils import timezone
 
+
 class Test(TestCase):
-  def create_question(self.ques, days):
-    time = timezone.now() + datetime.timedelta(days=days)
-    return Question.objects.create(self.ques=question_text, newTiem=time)
-
-
-
-
+    def create_question(ques, days):
+        time = timezone.now() + datetime.timedelta(days=days)
+        return Question.objects.create(ques=question_text, newTiem=time)
