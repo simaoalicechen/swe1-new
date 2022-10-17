9c378f78e4cab76a9a1a61c22f8a714d0c29dc3c
Alice Chen <alicechen005@gmail.com>
2022-10-17 13:47:21 -0400

    Added a test

--- polls/tests.py
+++ polls/tests.py
@@ -1,2 +1,14 @@
 # flake8: noqa
 from django.test import TestCase
+from django.utils import timezone
+from .models import Question
+from django.utils import timezone
+
+class Test(TestCase):
+  def create_question(self.ques, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(self.ques=question_text, newTiem=time)
+
+
+
+
